89f076c97d336e147eebcafe48d3e3eb6765b34d
Sadiq Okocha <sokocha@gmail.com>
2022-12-12 17:25:47 +0100

    SC9-1326: CONTROL_SCRIPT_PYTHON_AWS: Create_Bucket_with_Boto3_SADIQ_OKOCHA_Vers_1.9: creates bucket in terraform

--- stack-s3.tf
+++ stack-s3.tf
@@ -9,5 +9,15 @@ resource "aws_s3_bucket" "my-s3-bucket" {
 }
 
 
+resource "aws_s3_bucket_acl" "my-s3-bucket" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  acl    = "private"
+}
 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
